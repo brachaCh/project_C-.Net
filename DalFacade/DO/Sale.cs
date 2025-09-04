4fc0b74c871f951324691daf6ff8c3649f2ff03d
BatShevagit <sh0548503408@gmail.com>
2025-09-04 15:42:43 +0300

    try to solve bugs in sale

--- DalFacade/DO/Sale.cs
+++ DalFacade/DO/Sale.cs
@@ -6,17 +6,19 @@ public record Sale(
     double Price_Sale,
     bool If_All_Customers,
     DateTime Date_Start_Sale,
-
-    //double? Count_Sale = null,
     int Count_Sale,
-    DateTime? Date_End_Sale=null
-    )
+    DateTime? Date_End_Sale = null
+)
 {
- 
-    //public Sale()
-    //     : this(0, 0, 111, true, DateTime.Now, 1, null)
-    //{
+    // קונסטרקטור ריק ל-XmlSerializer
+    public Sale() : this(0, 0, 0, true, DateTime.Now, 1, null)
+    {
+    }
 
-    //}
+    // קונסטרקטור מותאם ליצירה ידנית
+    public Sale(int productId, double price_Sale, bool if_All_Customers,
+                DateTime date_Start_Sale, int count_Sale, DateTime? date_End_Sale = null)
+        : this(0, productId, price_Sale, if_All_Customers, date_Start_Sale, count_Sale, date_End_Sale)
+    {
+    }
 }
-
